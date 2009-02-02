64e7403d6c297bf9d9d48f514eb24c78bda5c18b
semente@taurinus.org <semente@taurinus.org>
2009-02-02 02:57:15 -0200

    Removing flatblocks package.

--- flatblocks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from flatblocks.models import FlatBlock
- 
-class FlatBlockAdmin(admin.ModelAdmin):
-    ordering = ['slug',]
-    list_display = ('slug', 'header', 'content')
-    search_fields = ('slug', 'header', 'content')
-
-admin.site.register(FlatBlock, FlatBlockAdmin)
