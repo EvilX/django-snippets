afda0c92242d17e61e46e57f3d219723a17ca367
Guilherme Gondim <semente@taurinus.org>
2009-10-27 17:59:31 -0200

    Minor change.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from snippets import get_version
 setup(
     name = 'django-snippets',
     version = get_version(),
-    description = 'Provides a templatetag for Django acting as an '
-                  '{% include %}, but the template content comes from '
-                  'database.',
-    long_description = 'django-snippets provides a templatetag called '
-                       'include_snippet for Django projects.\n\n'
-                       'include_snippet acts as an {% include %}, that loads '
-                       'a template and renders it with the current context, '
-                       'but the template content comes from database.',
+    description = ('Provides a templatetag for Django acting as an '
+                   '{% include %}, but the template content comes from '
+                   'database.'),
+    long_description = ('django-snippets provides a templatetag called '
+                        'include_snippet for Django projects.\n\n'
+                        'include_snippet acts as an {% include %}, that loads '
+                        'a template and renders it with the current context, '
+                        'but the template content comes from database.'),
     keywords = 'django apps template',
     license = 'New BSD License',
     author = 'Guilherme Gondim',
