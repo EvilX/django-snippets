2977dbf212b3e986f04f128a8ccadac39c8fe889
Guilherme Gondim <semente@taurinus.org>
2009-10-27 17:15:32 -0200

    Better content display in list_display.

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.utils.html import escape
+from django.utils.translation import ugettext_lazy as _
 from snippets.models import Snippet
 
 class SnippetAdmin(admin.ModelAdmin):
     ordering = ['slug',]
-    list_display = ('slug', 'content')
+    list_display = ('slug', '_get_content')
     list_filter = ('sites',)
     search_fields = ('slug', 'content')
+
+    def _get_content(self, obj):
+        tmpl = "<i>%s</i>"
+        if len(obj.content) > 385:
+            return (tmpl + " <b>[...]</b>") % escape(obj.content)[:385]
+        return tmpl % escape(obj.content)
+    _get_content.allow_tags = True
+    _get_content.short_description = _("content")
 
 admin.site.register(Snippet, SnippetAdmin)
